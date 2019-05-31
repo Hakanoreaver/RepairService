8b459d47c2cf00c9554ac7357d1c67672cdbba85
Hakanoreaver <hakanoreaver12@hotmail.com>
2019-05-31 12:24:13 +1000

    more UI added

--- src/main/java/com/CSCS314/RepairService/Repositories/CustomersRepository.java
+++ src/main/java/com/CSCS314/RepairService/Repositories/CustomersRepository.java
@@ -1,10 +1,12 @@
 package com.CSCS314.RepairService.Repositories;
 
         import com.CSCS314.RepairService.Repositories.Objects.Customers;
+        import org.springframework.data.jpa.repository.Modifying;
         import org.springframework.data.jpa.repository.Query;
         import org.springframework.data.repository.CrudRepository;
         import org.springframework.data.repository.query.Param;
         import org.springframework.stereotype.Repository;
+        import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CustomersRepository extends CrudRepository<Customers, Integer> {
@@ -13,4 +15,21 @@ public interface CustomersRepository extends CrudRepository<Customers, Integer> 
 
         @Query("SELECT c FROM Customers c WHERE c.customerId = :customerId")
         public Customers findById(@Param("customerId") int customerId);
+
+
+        @Modifying
+        @Transactional
+        @Query("UPDATE Customers c SET Name = :Name WHERE c.customerId = :customerId")
+        public void updateName(@Param("Name") String Name,@Param("customerId") Integer customerId);
+
+        @Modifying
+        @Transactional
+        @Query("UPDATE Customers c SET CardAuthToken = :CardAuthToken WHERE c.customerId = :customerId")
+        public void updateCardAuthToken(@Param("CardAuthToken") String CardAuthToken,@Param("customerId") Integer customerId);
+
+        @Modifying
+        @Transactional
+        @Query("UPDATE Customers c SET MobileNumber = :MobileNumber WHERE c.customerId = :customerId")
+        public void updateMobileNumber(@Param("MobileNumber") String MobileNumber,@Param("customerId") Integer customerId);
+
 }
